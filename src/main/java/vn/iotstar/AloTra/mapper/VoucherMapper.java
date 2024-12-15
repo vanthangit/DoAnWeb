2070f04c1cb5a8a1943331959e19c68d887d07d9
Thien Duy <duythien3624@gmail.com>
2024-12-15 20:51:05 +0700

    pull

--- src/main/java/vn/iotstar/AloTra/mapper/VoucherMapper.java
+++ src/main/java/vn/iotstar/AloTra/mapper/VoucherMapper.java
@@ -3,14 +3,15 @@ package vn.iotstar.AloTra.mapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
 import vn.iotstar.AloTra.dto.VoucherDTO;
+import vn.iotstar.AloTra.dto.VoucherUpdateDTO;
 import vn.iotstar.AloTra.entity.Voucher;
 
 @Mapper
 public interface VoucherMapper {
 
-    Voucher toVoucher(VoucherDTO voucherDTO);
+	Voucher toVoucher(VoucherDTO voucherDTO); 
     VoucherDTO toVoucherDTO(Voucher voucher);
 
     void updateVoucher(
-            @MappingTarget Voucher voucher, VoucherDTO voucherDTO);
+            @MappingTarget Voucher voucher, VoucherUpdateDTO voucherDTO);
 }
